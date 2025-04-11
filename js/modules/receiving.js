714a475768072d04a8f8be8354529f95bbddf22f
Gusseti <gustav@stadven.no>
2025-04-11 13:26:29 +0200

    Update receiving.js

--- js/modules/receiving.js
+++ js/modules/receiving.js
@@ -5,7 +5,7 @@ import { saveListsToStorage } from './storage.js';
 import { updateScannerStatus } from './ui.js';
 import { initCameraScanner, startCameraScanning, stopCameraScanning, connectToBluetoothScanner } from './scanner.js';
 import { openWeightModal } from './weights.js';
-import { importFromCSV, importFromJSON, importFromPDF, importFromReceiptPDF, importKvikFollgeseddel, exportList, exportWithFormat, exportToPDF } from './import-export.js';
+import { importFromCSV, importFromJSON, importFromPDF, importFromReceiptPDF, exportList, exportWithFormat, exportToPDF } from './import-export.js';
 
 
 // DOM elementer - Mottak
